0db4932163c63a80dd683820edc053b9032cad40
shashvin3 <161945999+shashvin3@users.noreply.github.com>
2025-08-12 16:43:18 +0000

    datatypes

---
+++ _basics/datatypes.js
@@ -0,0 +1,25 @@
+"use strict"; //treat all js code as newer version
+//alert(3+3) we are using nodejs, not browser
+
+//-------------Primitive Datatypes----------------
+
+// number => 2 to power 53
+// BigInt
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => value is not assigned yet
+// Symbol => unique
+
+let name = "Hanumanji"  //string
+let age = 18     //number
+let isreal = true   //boolean
+let change;    //undefined
+
+//------object------
+
+console.log(typeof "Assh")
+console.log(typeof 56)
+console.log(typeof true)
+console.log(typeof null)  //object
+console.log(typeof undefined) //udefined
