d3e3198e9dc9161b294f30d9589ea53533f0a041
shashvin3 <161945999+shashvin3@users.noreply.github.com>
2025-08-16 15:24:20 +0000

    string_Manipulation

--- _basics/string.js
+++ _basics/string.js
@@ -3,7 +3,7 @@ const age = 21;
 console.log(`My name is ${name} and age is ${age}`);
 console.log(name.length)
 
-const cartoonName = new String("Pokemon")  //By declaring string like this we can use its various method
+const cartoonName = new String("Pokemon-Sinchan-Slugterra")  //By declaring string like this we can use its various method
 
 console.log(cartoonName.length)
 console.log(cartoonName.toUpperCase())
@@ -15,3 +15,14 @@ console.log(newString)
 
 const newString1 = cartoonName.slice(-6,4)
 console.log(newString1)
+
+const newStringOne = "    Sheetal    "
+console.log(newStringOne)
+console.log(newStringOne.trim())
+
+const url = "https://sheetal.com/ash%20%ketchum"
+console.log(url.replace('%20%' , '-'))
+console.log(url.includes('forever'))
+
+console.log(cartoonName.split('-'))
+
