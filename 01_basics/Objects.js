5140c12d975483b09de2587961bbd9c8474e3c7f
shashvin3 <161945999+shashvin3@users.noreply.github.com>
2025-08-21 17:08:58 +0000

    Objects

--- 01_basics/Objects.js
+++ 01_basics/Objects.js
@@ -8,7 +8,7 @@ const MySymbol = Symbol("key1") //defining Symbol
 const JsUser = {
     name: "Ash",
     "full Name": "Ash Ketchum",  // we can access this key only using square bracket method
-    [MySymbol]: "My key",   //  Adding symbol as key in object
+    [MySymbol]: "My key",   //  Adding symbol as key in object  **we need to use square bracket to refer to a symbol
     age: 18,
     location: "faridabad",
     email:"ashketchum32003@gmail.com",
@@ -18,6 +18,24 @@ const JsUser = {
 
 //Two methods to access object elements
 console.log(JsUser.name);
+console.log(JsUser.email);
 console.log(JsUser["name"]);
 console.log(JsUser["full Name"]);
 console.log(JsUser[MySymbol]);
+
+JsUser.email ="ketchum.a24@gmail.com"; //You can change the key value like this
+console.log(JsUser.email);
+//Object.freeze(JsUser); //it will freeze the objects and no changes will propagate on it
+
+
+//we can declare the functions as variables in js
+
+JsUser.greeting = function(){
+    console.log("Hello Users")
+}
+JsUser.greetingTwo = function(){
+    console.log(`Hello Users, ${this.name} `)
+}
+
+console.log(JsUser.greeting());
+console.log(JsUser.greetingTwo());
